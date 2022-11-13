7971b3be4925eca1223164e9ae378d36e5fd738f
manpatinka <evgeniayurkova@gmail.com>
2022-11-13 14:03:46 +0200

    second commitment

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const allAnswers = ['No doubt!', 'Obviosly!', 'May be not this time', 'For now it\'s unpredictable.', 'Forget about it', 'No for now, but it may change.', 'You already know the answer yourself.'];
-let answersCount = allAnswers.length;
-const answer = () => {
+const allAnswers = ['No doubt!', 'Obviosly!', 'Yes, but not now', 'For now it\'s unpredictable.', 'Forget about it', 'It only depends on your actions', 'No for now, but it may change.', 'You already know the answer yourself.'];
+//const allAnswersRu = ['Даже не сомневайся!', 'Определенно.', 'Да, но не сейчас.', 'Сейчас нельзя дать точный ответ.', 'Забудь об этом.', 'Все зависит от твоих действий', 'Сейчас нет, но все может поменяться.', 'Ты уже знаешь ответ.'];
+
+
+let answer = () => {
+    let answersCount = allAnswers.length;
     let answerIndex = Math.floor(Math.random() * answersCount);
     return allAnswers[answerIndex];
 }
